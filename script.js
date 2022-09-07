804c6ad36d7078ffb10eb66a21a45eb1671329fb
David Tellez <davidctellezg@gmail.com>
2022-09-07 10:02:10 -0500

    Add functionality to save numbers and print result

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Here we have all our variables and selectors
 let barText = '';
-let displayNumber = 0;
+let operator = 'Duck'; //At the beginning or after clicking the clear button 
+                       //we put a nonsensical operator, to remind us of that
 let resultNumber = 0;
 let bar = document.querySelector('.bar');
 let numberButtons = document.querySelectorAll('.number');
@@ -22,21 +23,39 @@ numberButtons.forEach( (element) =>{
 //This parte will save the values and print the desired value
 operatorButtons.forEach( (element) => {
     element.addEventListener('click', () => {
-        //When an operator is clicked, the displayNumber changes its value to the displayed value.
-        barText = barText + ' ' + element.value + ' ';
-        bar.textContent = barText; //This code will be gone!
+        //If the user has typed nothing, we assume he wants to work with 0.
+        if (barText === ''){
+            barText = 0;
+        }
 
-        //Then displayValue and resultNumber are operated with the element.value.
+        //If it is the first time the user presses and operator, operator === Duck.
+        if (operator === 'Duck'){
+            resultNumber = Number(barText);
+            operator = element.value;    
+        }
+        //If the user press = it is the final result.
+        else if (element.value === '='){
+            bar.textContent = operate(operator, resultNumber, Number(barText));
+            operator = 'Duck';
+            resultNumber = 0;
+        }        
+        //If it is not the first time, operator !== Duck
+        else{
+            resultNumber = operate(operator, resultNumber, Number(barText));
+            bar.textContent = resultNumber;
+            operator = element.value;
+        }
 
-        //Change the resultNumber to the new result and change the DOM.
+        //We change barText so we can rewrite the results or the inputs on screen.
+        barText = '';
     })
 });
 
 
 //This part clears our screen.
 clearButton.addEventListener('click', () => {
+    operator = 'Duck';
     barText = '';
-    //Provisional.
     bar.textContent = '0';
 })
 
