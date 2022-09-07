56a43e1d50d0ce920df9b43c78ba47a5522ea007
David Tellez <davidctellezg@gmail.com>
2022-09-06 19:44:27 -0500

    Make scaffolding and basic calculator functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function sum(n1, n2){
+    return n1 + n2;
+}
+
+function subtract(n1, n2){
+    return n1 - n2;
+}
+
+function multiply(n1, n2){
+    return n1*n2;
+}
+
+function divide(n1, n2){
+    return n2 === 0? 'Syntax Error': n1/n2;
+}
